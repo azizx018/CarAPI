de20dc2a1cf1287200afa10ac1e37faaf40afcfc
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-04 14:56:34 -0500

    added 1 test to microservice

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,41 @@
 package com.udacity.pricing;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import com.udacity.pricing.domain.price.PriceRepository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PricingServiceApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private PriceRepository priceRepository;
+	private TestRestTemplate testRestTemplate;
 
 	@Test
 	public void contextLoads() {
 	}
+
+	@Test
+	public void getAllPrices() throws Exception {
+		ResponseEntity<String> response = this.testRestTemplate.getForEntity("https://localhost:" + port + "/prices", String.class);
+		assertThat(String.valueOf(response.getStatusCode()), equals(HttpStatus.OK));
+	}
+
 
 }
